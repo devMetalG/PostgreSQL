94ff8597e9dc3b020a5cddfb5368b50a7bda3453
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-27 20:06:49 -0600

    add: subQueries

---
+++ clases/08-group-by-having.sql
@@ -0,0 +1,10 @@
+SELECT
+    COUNT(*),
+    SUBSTRING(email, POSITION('@' IN email) + 1) AS domain
+FROM
+    users
+GROUP BY
+    SUBSTRING(email, POSITION('@' IN email) + 1)
+HAVING COUNT(*) > 1
+ORDER BY
+    COUNT(*) DESC;
