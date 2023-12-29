b15ee91a3c3740139eb2b937d07a5f5cb33c5980
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-28 20:19:12 -0600

    add: crear indices

---
+++ clases/14-crear-indices.sql
@@ -0,0 +1,13 @@
+SELECT
+    *
+FROM
+    country WHERE continent = 'Africa';
+
+CREATE UNIQUE INDEX "unique_country_name" ON country (
+	name
+);
+
+
+CREATE INDEX "country_continent" ON country (
+	continent
+);
