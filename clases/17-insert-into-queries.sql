736ad83697793ff899ae1e4305ed4609e281c491
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-29 20:13:42 -0600

    add: create table continents

---
+++ clases/17-insert-into-queries.sql
@@ -0,0 +1,16 @@
+SELECT
+    DISTINCT continent
+FROM
+    country
+ORDER BY continent ASC;
+
+INSERT INTO
+    continent (name)
+	SELECT
+	    DISTINCT continent
+	FROM
+	    country
+	ORDER BY
+	    continent ASC;
+
+SELECT * FROM continent;
