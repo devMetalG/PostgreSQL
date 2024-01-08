89bd42b0a17fecdc4eecfedf6f56d62efec27429
Guillermo Franco <fra_borjon@hotmail.com>
2024-01-08 00:30:56 -0600

    add: multiples joins

---
+++ clases/27-multiples-joins.sql
@@ -0,0 +1,21 @@
+SELECT * FROM countrylanguage WHERE isofficial = true;
+
+SELECT * FROM country;
+
+SELECT * FROM continent;
+
+-- idiomas oficiales que se hablan por continente
+
+SELECT a.language, c.name AS continent FROM countrylanguage a 
+INNER JOIN country b ON a.countrycode = b.code
+INNER JOIN continent c ON b.continent = c.code
+WHERE isofficial = true;
+
+-- cuantos idiomas oficiales se hablan por continente
+
+SELECT COUNT(*), continent FROM
+(SELECT DISTINCT a.language, c.name AS continent FROM countrylanguage a 
+INNER JOIN country b ON a.countrycode = b.code
+INNER JOIN continent c ON b.continent = c.code
+WHERE isofficial = true) AS totales
+GROUP BY continent;
